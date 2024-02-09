00e14b289627595df9c2d7ad760c097f22b83c55
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2024-02-09 10:32:04 +0100

    final

---
+++ public/script.js
@@ -0,0 +1,14 @@
+
+
+// alert("Hello World!");
+
+// visitekaart 2
+
+// const moreInfo = document.querySelector("circle")
+
+// const container = document.querySelector(".container")
+ 
+// moreInfo.addEventListener("click", () => {
+//  container.style.setProperty("visibility", "visible");
+// });
+
